20a1be3bf8c9c941bbc1d5f5906d814a770ffd4c
AlbaStarLord <cristian.gonzalez.alba@unillanos.edu.co>
2017-06-03 11:16:27 -0500

    Cambio crear aplicaciones estudiante y administrador

--- arru/arru/settings.py
+++ arru/arru/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.estudiante',
-    'apps.adminstrador',
+    'apps.administrador',
 ]
 
 MIDDLEWARE = [
