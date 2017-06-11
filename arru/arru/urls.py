efcf12d0c3f3d359b0e1b0ec11eaeb3583f21eb9
bayron.ortiz <bayrondanilo92@gmail.com>
2017-06-11 09:35:42 -0500

    Configuraciones para PythonAnywhere

--- arru/arru/urls.py
+++ arru/arru/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -21,4 +22,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^estudiante/', include('apps.estudiante.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
 
